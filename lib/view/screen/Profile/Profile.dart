0dd0764ed98c85bca45007ffddcce2c467a95e94
Shovon0017 <Shimantomostofa@gmail.com>
2024-09-15 00:13:15 +0600

    Pc_Shop

--- lib/view/screen/Profile/Profile.dart
+++ lib/view/screen/Profile/Profile.dart
@@ -45,80 +45,68 @@ class _ProfileState extends State<Profile> {
               )
             ],
           ),
-          Padding(
-            padding:
-            const EdgeInsets.only(left: 20, right: 20, top: 5, bottom: 5),
-            child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    minimumSize: const Size(29, 58),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10))),
-                onPressed: () {
+          ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(29, 58),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10))),
+              onPressed: () {
 
-                },
-                child: const Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "Requested Order",
-                      style: TextStyle(fontSize: 18, color: Colors.black),
-                    ),
-                    Icon(
-                      Icons.arrow_forward_ios,
-                      color: Colors.black,
-                    ),
-                  ],
-                )),
-          ),
-          Padding(
-            padding:
-            const EdgeInsets.only(left: 20, right: 20, top: 5, bottom: 5),
-            child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    minimumSize: const Size(29, 58),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10))),
-                onPressed: () {
+              },
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Requested Order",
+                    style: TextStyle(fontSize: 18, color: Colors.black),
+                  ),
+                  Icon(
+                    Icons.arrow_forward_ios,
+                    color: Colors.black,
+                  ),
+                ],
+              )),
+          ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(29, 58),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10))),
+              onPressed: () {
 
-                },
-                child: const Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "Confirm Order",
-                      style: TextStyle(fontSize: 18, color: Colors.black),
-                    ),
-                    Icon(
-                      Icons.arrow_forward_ios,
-                      color: Colors.black,
-                    ),
-                  ],
-                )),
-          ),
-          Padding(
-            padding:
-            const EdgeInsets.only(left: 20, right: 20, top: 5, bottom: 5),
-            child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    minimumSize: const Size(29, 58),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10))),
-                onPressed: () {
-                },
-                child: const Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "Update Profile",
-                      style: TextStyle(fontSize: 18, color: Colors.black),
-                    ),
-                    Icon(
-                      Icons.arrow_forward_ios,
-                      color: Colors.black,
-                    ),
-                  ],
-                )),
-          ),
+              },
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Confirm Order",
+                    style: TextStyle(fontSize: 18, color: Colors.black),
+                  ),
+                  Icon(
+                    Icons.arrow_forward_ios,
+                    color: Colors.black,
+                  ),
+                ],
+              )),
+          ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(29, 58),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10))),
+              onPressed: () {
+              },
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Update Profile",
+                    style: TextStyle(fontSize: 18, color: Colors.black),
+                  ),
+                  Icon(
+                    Icons.arrow_forward_ios,
+                    color: Colors.black,
+                  ),
+                ],
+              )),
           CommonButton(buttonName: "LogOut", onTap: (){
             controller.logOutFun();
           })
