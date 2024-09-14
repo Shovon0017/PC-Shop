4aae0e0a844ce03a1b431251a66d9dc730694dfe
Shovon0017 <Shimantomostofa@gmail.com>
2024-09-15 02:30:44 +0600

    Pc_Shop

--- lib/view/screen/Profile/Profile.dart
+++ lib/view/screen/Profile/Profile.dart
@@ -32,50 +32,76 @@ class _ProfileState extends State<Profile> {
           )
         ],
       ),
-      body: Column(
-        children: [
-          Text("Profile",style: TextStyle(
-            fontWeight: FontWeight.bold,fontSize: 30
-          ),),
-           Row(
-            children: [
-              CircleAvatar(
-                radius: 35,backgroundImage: AssetImage("images/Shovonpic.jpg"),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "MD Shimanto Mostofa",
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    "shovon@gmail.com",
-                  )
-                ],
-              )
-            ],
-          ),
-          CommonButton(buttonName: "Requested Order", onTap: (){
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal:10),
+        child: Column(
+          children: [
+            Text("Profile",style: TextStyle(
+              fontWeight: FontWeight.bold,fontSize: 30
+            ),),
+             SizedBox(height: 120,
+               width: double.infinity,
+               child: Card(
+                 elevation: 5,
+                 color: Color(0xff9a0000),
+                 child: Row(crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    SizedBox(
+                      height: 100,
+                        width: 100,
+                        child: Card(child: Image.asset("images/Shovonpic.jpg"))),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Name:Md Shimanto Mostofa",
+                          style: TextStyle(fontSize: 20,color: Colors.white, fontWeight: FontWeight.bold),
+                        ),
+                        RichText(text: TextSpan(
+                          children: [
+                            TextSpan(text: "Email:",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold,color: Colors.white)),
+                            TextSpan(text: "Shovon@gmail.com",style: TextStyle(color: Colors.white,fontSize: 15))
+                          ]
+                        ))
+                      ],
+                    )
+                  ],
+                             ),
+               ),
+             ),SizedBox(
+              height: 30,
+            ),
+            CommonButton(
+                buttonName: "Update Profile", onTap: (){
+            }),
 
-          }),
-          CommonButton(buttonName: "Confirm Order", onTap: (){
+            CommonButton(buttonName: "Requested Order", onTap: (){
 
-          }),
-          CommonButton(buttonName: "Order Report", onTap: (){
+            }),
+            CommonButton(buttonName: "Confirm Order", onTap: (){
 
-          }),
-          CommonButton(buttonName: "Update Profile", onTap: (){
+            }),
+            CommonButton(
 
-          }),
-          CommonButton(buttonName: "Setting", onTap: (){
+                buttonName: "Order Report", onTap: (){
 
-          }),
+            }),
 
-          CommonButton(buttonName: "LogOut", onTap: (){
-            controller.logOutFun();
-          })
-        ],
+            CommonButton(buttonName: "Setting", onTap: (){
+
+            }),
+            CommonButton(buttonName: "Support", onTap: (){
+
+            }),
+            CommonButton(buttonName: "FAQ", onTap: (){
+
+            }),
+            CommonButton(buttonName: "LogOut", onTap: (){
+              controller.logOutFun();
+            })
+          ],
+        ),
       ),
     );
   }
