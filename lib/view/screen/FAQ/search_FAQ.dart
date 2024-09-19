fb6075bdea34ce09407c225feedb32260ecc6eee
Shovon0017 <Shimantomostofa@gmail.com>
2024-09-20 03:47:24 +0600

    Pc_Shop

--- lib/view/screen/FAQ/search_FAQ.dart
+++ lib/view/screen/FAQ/search_FAQ.dart
@@ -23,11 +23,7 @@ class SearchFieldFAQ extends StatelessWidget {
               borderSide:  const BorderSide(color: Colors.black),
               borderRadius: BorderRadius.circular(8)),
           labelText: "Search FAQ",
-          suffixIcon: InkWell(
-              onTap: (){
-
-              },
-              child: const Card(color:Colors.blue,child: Icon(Icons.search,color: Colors.white,)))
+          suffixIcon: const Card(color:Colors.blue,child: Icon(Icons.search,color: Colors.white,))
       ),
     );
   }
