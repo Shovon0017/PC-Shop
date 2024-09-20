a3dda6e4be6636f633cf0857eba1bb2ab7c337cc
Shovon0017 <Shimantomostofa@gmail.com>
2024-09-20 22:28:31 +0600

    Pc_Shop

--- lib/view/screen/updateProfile/update_profile.dart
+++ lib/view/screen/updateProfile/update_profile.dart
@@ -7,6 +7,8 @@ class UpdateProfile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+    );
   }
 }
