ca82bb01cea20a301433cfbe55fe6cf22d649548
Shovon0017 <Shimantomostofa@gmail.com>
2024-09-21 21:18:25 +0600

    Pc_Shop

--- lib/view/screen/updateProfile/update_profile.dart
+++ lib/view/screen/updateProfile/update_profile.dart
@@ -1,6 +1,9 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:pc_mart/common%20widget/CommonIcon.dart';
+import 'package:pc_mart/view/screen/notification/notification.dart';
 
 class UpdateProfile extends StatelessWidget {
   const UpdateProfile({super.key});
@@ -8,9 +11,21 @@ class UpdateProfile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("UPDATE PROFILE"),
+      appBar:  AppBar(
+        backgroundColor: const Color(0xffFFFFFF),
+        title: SizedBox(
+            height: 32,
+            width: 114,
+            child: Image.asset("images/pcmart.jpg")),
         centerTitle: true,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 10),
+            child: CommonIconButton(onTap: (){
+              Get.to(()=>const NotificationShow());
+            }),
+          )
+        ],
       ),
     );
   }
